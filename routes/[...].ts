780119da45779a5406806e1ba31f62a6e6da9e1d
Johann Schopplich <mail@johannschopplich.com>
2022-06-02 10:52:37 +0200

    refactor: public `favicon.svg`

--- routes/[...].ts
+++ routes/[...].ts
@@ -6,7 +6,7 @@ export default defineEventHandler(() => {
   <html lang="en">
     <head>
       <meta charset="utf-8">
-      <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐣</text></svg>" />
+      <link rel="icon" href="/favicon.svg" />
       <title>Nitro Test Deployment</title>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico/css/pico.min.css">
     </head>
