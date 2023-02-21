9a315a292d2f795478888c5a0ec14152294a38aa
Pooya Parsa <pooya@pi0.io>
2023-02-21 15:44:49 +0100

    update rendered page design

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,4 +1,12 @@
 import { defineNitroConfig } from 'nitropack'
 
 export default defineNitroConfig({
+  renderer: './renderer',
+  publicAssets: [
+    {
+      baseURL: '/dist',
+      dir: './public/dist',
+      maxAge: 60 * 60 * 24 * 365
+    }
+  ]
 })
