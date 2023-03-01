46f4756501ce2b7b45b1ccab6cb88372aec27338
Pooya Parsa <pooya@pi0.io>
2023-03-01 13:19:29 +0100

    register /assets public dir

--- nitro.config.ts
+++ nitro.config.ts
@@ -10,6 +10,11 @@ export default defineNitroConfig({
   },
   publicAssets: [
     {
+      baseURL: '/assets',
+      dir: './public/assets',
+      maxAge: 60 * 60 * 24 * 365
+    },
+    {
       baseURL: '/_dist',
       dir: './public/_dist',
       maxAge: 60 * 60 * 24 * 365
