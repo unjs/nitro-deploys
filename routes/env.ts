034fac43dd8ad71214d41ed9c16978ded795f433
Pooya Parsa <pooya@pi0.io>
2023-03-10 15:31:01 +0100

    fix: use safe env filter

--- routes/env.ts
+++ routes/env.ts
@@ -1,5 +1,11 @@
 export default eventHandler(() => {
   return {
-    processEnv: process?.env || "<no env>"
+    processEnv: safeEnv(process.env)
   }
 })
+
+const tokenRe = /password|token|key|secret/i
+
+function safeEnv(env: Record<string, string> = {}) {
+  return Object.fromEntries(Object.entries(env).filter(([key]) => !tokenRe.test(key)))
+}
