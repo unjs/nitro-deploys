185a99d4ea68be8e2116007fa5abb206a44c23ea
Pooya Parsa <pooya@pi0.io>
2024-09-11 18:55:13 +0200

    update index

--- server/routes/[...path].ts
+++ server/routes/[...path].ts
@@ -6,6 +6,15 @@ const getURL = (p) => baseURL + p.replace(/^\//, "");
 
 const routes = ["/api/hello", "/api/env", "/stream"];
 
+if (import.meta.dev) {
+  deployments.unshift({
+    name: "dev",
+    url: "http://localhost:3000",
+    dash: "",
+    docs: "",
+  });
+}
+
 export default defineRenderHandler((event) => {
   const url = getRequestURL(event) as URL;
   const currentDeployment =
@@ -13,102 +22,106 @@ export default defineRenderHandler((event) => {
     ({} as (typeof deployments)[number]);
 
   const body = /* html */ `<!doctype html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8" />
-        <title>Nitro Test Deployment</title>
-        <link rel="icon" href="/nitro.svg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script src="${getURL("/_dist/tailwind@3.4.5.js")}"></script>
-      </head>
+  <html lang="en">
 
-      <body class="bg-neutral-900">
-        <div class="flex justify-center items-center h-screen">
-          <div class="border border-gray-200 text-white p-8 rounded-lg max-w-lg">
-            <h1 class="text-3xl font-bold mb-4">
-              <img src="${getURL("/nitro.svg")}" class="w-8 h-8 inline-block" />
-              <a href="${getURL("/")}">Nitro Test Deployment</a>
-            </h1>
-            <div class="mb-3">
-              Current route: ${event.path}
-            </div>
-            <div class="mb-3">
-              <ul class="list-disc">
-                ${routes
-                  .map(
-                    (link) => /* html */ ` <li>
-                        <a href="${getURL(link)}" class="underline">${link}</a>
-                      </li>`,
-                  )
-                  .join("\n")}
-              </ul>
-            </div>
-            <div class="mt-3 pt-3 border-t-2">
-              Performances:
-              <table class="table-auto" id="perf"></table>
-            </div>
-            <!-- Deployments list -->
-            <div class="border-t-2 mt-3 pt-3">
-              Current preset: ${currentDeployment?.name || "(custom)"} (<a
-                class="underline"
-                href="${currentDeployment?.url}"
-                >docs</a
-              >)
-              <br />
-              <br />
+  <head>
+    <meta charset="utf-8" />
+    <title>Nitro Test Deployment</title>
+    <link rel="icon" href="/nitro.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <script src="${getURL("/_dist/tailwind@3.4.5.js")}"></script>
+  </head>
+
+  <body class="bg-neutral-900">
+    <div class="flex justify-center items-center h-screen">
+      <div class="border border-gray-200 text-white p-8 rounded-lg max-w-lg">
+        <!-- Title -->
+        <h1 class="text-3xl font-bold mb-4">
+          <img src="${getURL("/nitro.svg")}" class="w-8 h-8 inline-block" />
+          <a href="${getURL("/")}">Nitro Test Deployment</a>
+        </h1>
+
+        <!-- Perf -->
+        <div class="mb-3 pt-3">
+          <table class="table-auto" id="perf"></table>
+        </div>
+
+        <!-- Routes -->
+        <div class="mb-3 border-t-1">
+          <p>Current route: ${event.path}</p>
+          <ul style="list-style: circle">
+            ${routes
+              .map(
+                (link) => /* html */ ` <li>
+              <a href="${getURL(link)}" class="underline">${link}</a>
+            </li>`,
+              )
+              .join("\n")}
+          </ul>
+        </div>
+
+        <!-- Footer -->
+        <div class="mt-3 pt-3">
+          <!-- Deployment -->
+          <div>
+            <select onchange="window.location.href=this.value" value="${currentDeployment.url}" id="countries"
+              class="mt-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               ${deployments
                 .map((d) =>
-                  d.enabled
-                    ? /* html */ ` <a
-                        href="${d.url}"
-                        class="underline ${
-                          d.name === currentDeployment.name ? "font-bold" : ""
-                        }"
-                        >${d.name}</a
-                      >`
-                    : /* html */ ` <span class="text-gray-200">${d.name}</span>`,
+                  d.url
+                    ? /* html */ ` <option value="${d.url}">${d.name}</option>`
+                    : /* html */ `<option disabled>${d.name}</option>`,
                 )
-                .join(" | ")}
-            </div>
-            <!-- Footer -->
-            <div class="mt-3 pt-3 border-t-2">
-              Generated at ${new Date().toUTCString()} with
-              <a
-                href="https://nitro.unjs.io/"
-                class="underline"
-                target="_blank"
-                rel="noopener"
-                >nitro</a
-              ><span class="text-gray-200"
-                >@${useRuntimeConfig().nitroVersion}</span
-              >
-              <a
-                href="https://github.com/unjs/nitro-deploys"
-                class="underline"
-                target="_blank"
-                rel="noopener"
-                >(source code)</a
-              >
-            </div>
+                .join("\n")}
+            </select>
+          </div>
+          <div class="mt-2">
+          <p>Generated at ${new Date().toUTCString()}</p>
+            <p>
+              <a href="https://nitro.unjs.io/" class="underline" target="_blank" rel="noopener">Nitro</a><span
+                class="text-gray-200">@${useRuntimeConfig().nitroVersion}</span>
+            </p>
+            <p class="text-center">
+              <a href="https://github.com/unjs/nitro-deploys" class="underline" target="_blank" rel="noopener">source code</a>
+              </a>
+            </p>
           </div>
         </div>
-        <script>
-          const timing = window.performance.timing;
-          const measures = {
-            "Connection time:": timing.connectEnd - timing.connectStart,
-            "DNS lookup:": timing.domainLookupEnd - timing.domainLookupStart,
-            "TTFB:": timing.responseStart - timing.requestStart,
-            Response: timing.responseEnd - timing.responseStart,
-          };
-          document.querySelector("#perf").innerHTML = Object.entries(measures)
-            .map(
-              ([name, value]) =>
-                "<tr><td>" + name + "</td><td>" + value + "ms</td></tr>",
-            )
-            .join("");
-        </script>
-      </body>
-    </html>`;
+      </div>
+    </div>
+    <script>
+      const perfNavTiming = window.performance.getEntriesByType('navigation')[0];
+      const renderPerfStats = () => {
+        const measure = (end, start) => {
+          const diff = end - start;
+          return diff >= 0 ? Math.round(diff * 1000) / 1000 + " ms" : "-";
+        }
+        console.log(perfNavTiming.duration);
+        const measures = {
+          Protocol: perfNavTiming.nextHopProtocol,
+          Transfer: perfNavTiming.transferSize + " bytes",
+          Request: measure(perfNavTiming.responseEnd, perfNavTiming.requestStart),
+          Duration: measure(perfNavTiming.duration, 0),
+        };
+        document.querySelector("#perf").innerHTML = Object.entries(measures)
+          .map(
+            ([name, value]) =>
+              "<tr><td>" + name + ": " + "</td><td>" + value + "</td></tr>",
+          )
+          .join("");
+      };
+      renderPerfStats();
+      const int = setInterval(() => {
+        if (perfNavTiming.loadEventEnd) {
+          clearInterval(int);
+          renderPerfStats();
+        }
+      });
+    </script>
+  </body>
+
+  </html>
+  `;
 
   return {
     body,
